2f9382d04070703fa7402cd773a0374f7d1ad7dc
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-03-25 11:13:26 +0300

    Add loader for when posts are being fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { useState, useEffect, useRef } from "react";
-import { Search } from "lucide-react";
-import "./App.css";
-import ArticlesList from "./Components/ArticlesList";
 import { Outlet, useNavigate } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [articlesOrder, setArticlesOrder] = useState("all");
-  const [darkMode, setDarkMode] = useState(true);
+  const [loading, setLoading] = useState(false); // New loading state
   const linksRef = useRef(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch(`http://localhost:4000/articles/${articlesOrder}`) // Adjust API URL as needed
-      .then((response) => response.json())
-      .then((data) => setArticles(data.articles)) // Save articles in state
-      .catch((error) => console.error("Error fetching articles:", error));
-  }, [articlesOrder]);
-  console.log(articles);
+    const fetchArticles = async () => {
+      setLoading(true); // Start loading
 
-  useEffect(() => {
-    document.body.classList.toggle("light", darkMode);
-  }, [darkMode]);
+      try {
+        const response = await fetch(
+          `http://localhost:4000/articles/${articlesOrder}`,
+        );
+        const data = await response.json();
+        setArticles(data.articles);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      } finally {
+        setLoading(false); // Stop loading
+      }
+    };
+
+    fetchArticles();
+  }, [articlesOrder]);
 
   const activateLink = (e) => {
     e.preventDefault();
@@ -30,19 +37,13 @@ function App() {
       const links = linksRef.current.querySelectorAll("a");
       navigate("/");
 
-      links.forEach((link) => {
-        link.classList.remove("active");
-      });
+      links.forEach((link) => link.classList.remove("active"));
 
       if (e.target.tagName === "H2") {
         e.target.parentNode.classList.add("active");
       }
       setArticlesOrder(e.target.textContent.toLowerCase());
     }
-  };
-
-  const handleTheme = () => {
-    setDarkMode(!darkMode);
   };
 
   const searchArticles = (e) => {
@@ -57,33 +58,7 @@ function App() {
           <h1>Postly</h1>
         </a>
       </nav>
-      <div className="sidebar">
-        <h1>Search</h1>
-        <form>
-          <input type="text" id="search-articles" onChange={searchArticles} />
-          <button>
-            {/* <Search /> */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-search"
-            >
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.3-4.3" />
-            </svg>
-          </button>
-        </form>
-        <button className="theme-btn" onClick={handleTheme}>
-          Theme: {!darkMode ? "Dark" : "Light"}
-        </button>
-      </div>
+      <Sidebar searchArticles={searchArticles} />
       <main className="main">
         <div className="pages" ref={linksRef}>
           <a href="" onClick={activateLink} className="active">
@@ -96,16 +71,14 @@ function App() {
             <h2>Admin</h2>
           </a>
         </div>
-        <Outlet context={articles} />
+
+        {/* Show Loading Spinner when fetching */}
+        {loading ? (
+          <div className="loader"></div>
+        ) : (
+          <Outlet context={articles} />
+        )}
       </main>
-      {/* <footer>
-        <p>
-          Developed by <span>@Zaid0412</span>
-        </p>
-        <a href="https://github.com/Zaid0412/Blog-Api-Backend">
-          View on GitHub
-        </a>
-      </footer> */}
     </div>
   );
 }
--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
 
